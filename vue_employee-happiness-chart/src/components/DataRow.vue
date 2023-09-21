e7b31a2c331ef368a71bf546367f850e10e31ef8
Patrick <savvyscheme@gmail.com>
2023-09-21 08:24:05 -0500

    Added data input field component

---
+++ vue_employee-happiness-chart/src/components/DataRow.vue
@@ -0,0 +1,22 @@
+<script setup>
+    defineProps({
+        data: {
+            type: Object,
+        }
+    })
+</script>
+
+<template>
+    <div class="dataRow">
+      <p class="departmentTitle">{{ data.department }}</p>
+      <input type="text" :value="data.very_happy">
+      <input type="text" :value="data.happy">
+      <input type="text" :value="data.content">
+      <input type="text" :value="data.unhappy">
+      <input type="text" :value="data.very_unhappy">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
