231ef6814263e7b43d9d7599d529eb30c521972f
Patrick <savvyscheme@gmail.com>
2023-09-26 08:17:18 -0500

    Added Graph data edit and validation

--- lumen-app/app/Http/Controllers/HappinessChartController.php
+++ lumen-app/app/Http/Controllers/HappinessChartController.php
@@ -5,13 +5,51 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use App\Models\Department;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
 
 
 class HappinessChartController extends BaseController
 {
     public function getAllDepartmentsHappiness() {
         $departments = Department::all();
-        // var_dump($departments);
         return response()->json($departments);
+    }
+
+    public function postAllDepartmentsHappiness(Request $request) {
+        $jsonData = $request->json()->all();
+        $graphData = $jsonData['graph'];
+
+        // Define the validation rules
+        $validator = Validator::make($jsonData, [
+            'graph.*.id' => 'required|exists:happiness_chart,id', 
+            'graph.*.very_happy' => 'required|numeric|min:0',
+            'graph.*.happy' => 'required|numeric|min:0',
+            'graph.*.content' => 'required|numeric|min:0',
+            'graph.*.unhappy' => 'required|numeric|min:0',
+            'graph.*.very_unhappy' => 'required|numeric|min:0',
+        ]);
+
+        // If the validation fails, return a JSON response with the errors
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+            // return response()->json(['status' => 'failed']);
+        }
+
+        foreach($graphData as $deparment) {
+            $department = Department::findOrFail($deparment['id']);
+            $data = $request->only([
+                'very_happy', 'happy', 'content', 'unhappy', 'very_unhappy'
+            ]);
+            $department->update($deparment);
+        }
+
+        Log::info('Received Data:', $graphData);
+
+        return response()->json(['status' => 'success']);
+
     }
 }
