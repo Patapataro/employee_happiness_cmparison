e8ff5c8bb121f4d86397dd6ca4b6696a1d50005f
Patrick <savvyscheme@gmail.com>
2023-09-26 08:12:37 -0500

    Updated vite routes

--- vue_employee-happiness-chart/vite.config.js
+++ vue_employee-happiness-chart/vite.config.js
@@ -12,5 +12,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      },
+    }
   }
 })
